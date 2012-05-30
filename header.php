1cd90d2d5b8cae7362afe39dc434a80e85d8ff7b
Michael Victor <michaelism21@gmail.com>
2012-05-30 16:30:17 +0530

    Bootstrap Files being loaded on every page

--- header.php
+++ header.php
@@ -2,6 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link href="css/main.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet/less" href="less/style.less">
+<script src="js/libs/less-1.3.0.min.js"></script>
 <link rel=”image_src” href=”http://mediaget.info/images/mediaget.jpg” />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
@@ -15,6 +17,7 @@ $(document).ready(function() {
 	});
 
 </script>
+<script src="js/libs/bootstrap/bootstrap.min.js"></script>
 <script type="text/javascript" src="http://radarurl.com/js/radarurl_widget.js"></script><script type="text/javascript">radarurl_call_radar_widgetv2({edition:"Dynamic",location:"bottomleft"})</script><noscript><a href="http://radarurl.com/">RadarURL</a></noscript>
 </head>
 
